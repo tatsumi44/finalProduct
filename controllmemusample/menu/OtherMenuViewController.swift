7571f7c4ab732309ae3d0d5d135ce34305f0b26a
tatsumi kentaro <tatsuken4290@gmail.com>
2018-03-06 17:24:19 +0900

    add profile photos

--- controllmemusample/menu/OtherMenuViewController.swift
+++ controllmemusample/menu/OtherMenuViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Firebase
-class OtherMenuViewController: UIViewController,UITableViewDataSource {
+class OtherMenuViewController: UIViewController,UITableViewDataSource,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     
     //異なるStorybordに画面遷移するのでこれを用いる、nameでStorybordの名前を指定
@@ -17,16 +17,14 @@ class OtherMenuViewController: UIViewController,UITableViewDataSource {
     var profielArray = [String]()
     var db: Firestore!
     
+    
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var mainTableView: UITableView!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         mainTableView.dataSource = self
-        imageView.image = UIImage(named: "IMG_1827.png")
         
         // Do any additional setup after loading the view.
     }
@@ -36,6 +34,8 @@ class OtherMenuViewController: UIViewController,UITableViewDataSource {
         let user = Auth.auth().currentUser
         uid = user?.uid
         db = Firestore.firestore()
+        let ref = Storage.storage().reference()
+        //db接続
         if user != nil{
             db.collection("users").document(uid).getDocument { (snap, error) in
                 let data = snap?.data()
@@ -52,7 +52,22 @@ class OtherMenuViewController: UIViewController,UITableViewDataSource {
             present(nextView!, animated: true, completion: nil)
             print("ログインいるで")
         }
-        
+        db.collection("users").document(uid).getDocument { (snap, error) in
+            if let error = error{
+                print("error")
+            }else{
+                let data = snap?.data()
+                let profilePath = data!["profilePath"] as! String
+                ref.child("image").child("profile").child(profilePath).downloadURL { url, error in
+                    if let error = error {
+                        // Handle any errors
+                    } else {
+                        //imageViewに描画、SDWebImageライブラリを使用して描画
+                        self.imageView.sd_setImage(with: url!, completed: nil)
+                    }
+                }
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -84,8 +99,52 @@ class OtherMenuViewController: UIViewController,UITableViewDataSource {
         } catch let signOutError as NSError {
             print ("Error signing out: %@", signOutError)
         }
-        
-        
     }
+    
+    
+    @IBAction func choiceLibraly(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            picker.allowsEditing = true
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerEditedImage] as? UIImage
+    
+        let date = NSDate()
+        //時間を文字列に整形
+        let format = DateFormatter()
+        format.dateFormat = "yyyy_MM_dd_HH_mm_ss"
+        //整形した文字列を画像のpathに整形
+        let datePath = format.string(from: date as Date)
+        
+        db.collection("users").document(uid).updateData(["profilePath" : "\(datePath)_\(uid!).jpg"]) { (error) in
+            if let error = error{
+                print("error")
+                
+            }else{
+                let data: Data = UIImageJPEGRepresentation(image!, 0.1)!
+                //ストレージの保存先のpathを指定
+                let ref = Storage.storage().reference()
+                let imagePath = ref.child("image").child("profile").child("\(datePath)_\(self.uid!).jpg")
+                let uploadTask = imagePath.putData(data, metadata: nil) { (metadata, error) in
+                    guard let metadata = metadata else {
+                        // Uh-oh, an error occurred!
+                        return
+                    }
+                    // Metadata contains file metadata such as size, content-type, and download URL.
+                    let downloadURL = metadata.downloadURL
+                    print(downloadURL)
+                }
+            }
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
     
 }
